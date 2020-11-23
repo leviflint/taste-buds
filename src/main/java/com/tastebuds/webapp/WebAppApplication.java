d39bd0939dcff7c983b8245ba4c6fa4e604bf73e
Abigail McClaine <mcclaineabigail@gmail.com>
2020-11-23 10:29:32 -0500

    correct startup

--- src/main/java/com/tastebuds/webapp/WebAppApplication.java
+++ src/main/java/com/tastebuds/webapp/WebAppApplication.java
@@ -1,13 +1,13 @@
-package tastebuds.demo;
+package com.tastebuds.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WebAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(WebAppApplication.class, args);
     }
 
 }
--- src/test/java/com/tastebuds/webapp/WebAppApplicationTests.java
+++ src/test/java/com/tastebuds/webapp/WebAppApplicationTests.java
@@ -1,10 +1,10 @@
-package tastebuds.demo;
+package com.tastebuds.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class WebAppApplicationTests {
 
     @Test
     void contextLoads() {
