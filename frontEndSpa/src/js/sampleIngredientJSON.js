22d2b1c9f4b19bcff75a68bbf8547df63e0a7239
leviflint <tweekex@hotmail.com>
2020-11-25 10:36:06 -0500

    Made sample JSON

---
+++ frontEndSpa/src/js/sampleIngredientJSON.js
@@ -0,0 +1,25 @@
+const allIngredients = [ {
+    
+    "ingredient": "chicken",
+    "id": 1,
+    "pairings": [
+      {
+        "pairing": "garlic",
+        "id": 6
+      },
+      {
+        "pairing": "teriyaki sauce",
+        "id": 5
+      },
+      {
+        "pairing": "lemon",
+        "id": 4
+      }
+    ],
+    "affinity1": "garlic, pasta, pesto",
+    "affinity2": "rice, beans, cheese",
+    "affinity3": "spinach, strawberry, poppy seed dressing"
+  }
+]
+
+export { allIngredients }
