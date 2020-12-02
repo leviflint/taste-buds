d13fc8040e434979b71e6ec5f00009ecabce2e7b
David Granados <davidcolumbus2@gmail.com>
2020-12-02 09:26:25 -0500

    pushed work from yesterday

--- backEndApi/src/test/java/com/tastebuds/webapp/JPAWiringTest.java
+++ backEndApi/src/test/java/com/tastebuds/webapp/JPAWiringTest.java
@@ -39,9 +39,9 @@ public class JPAWiringTest {
     public void ingredientsHaveManyPairings() {
         Ingredient testIngredient = new Ingredient("chicken","1","2","3");
         ingredientRepo.save(testIngredient);
-        Pairing testPairing1 = new Pairing("salt", 2,testIngredient);
+        Pairing testPairing1 = new Pairing("salt", Pairing.DietaryFilter.SALTY,2,testIngredient);
         pairingRepo.save(testPairing1);
-        Pairing testPairing2 = new Pairing( "pepper", 2, testIngredient);
+        Pairing testPairing2 = new Pairing( "pepper", Pairing.DietaryFilter.SMOKY,2, testIngredient);
         pairingRepo.save(testPairing2);
         flushClear();
         Ingredient retrieveIngredient = ingredientRepo.findById(testIngredient.getId()).get();
@@ -53,7 +53,7 @@ public class JPAWiringTest {
         ingredientRepo.save(testIngredient);
         Ingredient testIngredient2 = new Ingredient("cinnamon","1","2","3");
         ingredientRepo.save(testIngredient2);
-        Pairing testPairing1 = new Pairing( "butter",3, testIngredient, testIngredient2);
+        Pairing testPairing1 = new Pairing( "butter", Pairing.DietaryFilter.BLAND,3, testIngredient, testIngredient2);
         pairingRepo.save(testPairing1);
         flushClear();
         Pairing retrievePairings = pairingRepo.findById(testPairing1.getId()).get();
