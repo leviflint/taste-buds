babbbca713707ac07533b9a78f63710fabdd31fb
unknown <michie_tiff@hotmail.com>
2020-12-03 10:48:11 -0500

    submit button not working

--- frontEndSpa/src/js/blog.js
+++ frontEndSpa/src/js/blog.js
@@ -1,9 +1,9 @@
-import {displayPosts} from "./post.js"
+import { displayPosts } from "./post.js"
 
 
 
 
-fetch("http://localhost:8080/api/post", {
+fetch("http://localhost:8080/api/posts", {
         method: "GET",
         mode: "cors",
         headers: {
@@ -15,4 +15,34 @@ fetch("http://localhost:8080/api/post", {
     // .then((mainElement) => mainElement.appendChild(displayPosts))
     .catch((error) => console.log(error));
 
-    displayPosts()
+displayPosts()
+
+let button = document.getElementById("submit");
+let ingredientInput = document.getElementById("dropdown").value;
+let hashtagInput = document.getElementById("hashtag").value;
+let recipeNameInput = document.getElementById("recipe-name").value;
+let linkInput = document.getElementById("link").value;
+let photoInput = document.getElementById("photo").value;
+
+
+button.addEventListener("click", () => {
+
+    const postJson = {
+        "ingredient": ingredientInput,
+        "recipeName": recipeNameInput,
+        "recipeLink": linkInput,
+        "hashtag": hashtagInput,
+        "photo": photoInput
+    }
+    fetch("http://localhost:8080/api/post", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postJson)
+        })
+        .then(response => response.json())
+        .catch(err => console.error(err))
+        .then(posts => displayPosts(posts))
+        .then(location.reload());
+})
