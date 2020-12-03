0e1bb4c3e8e452ea9f2e5d3a6219ccb4b55b6968
Alyssha Timm <aly.nelso@gmail.com>
2020-12-03 11:32:33 -0500

    Started GetMapping for viewing ingredients associated with pairing by name

--- backEndApi/src/main/java/com/tastebuds/webapp/Storage/PairingStorage.java
+++ backEndApi/src/main/java/com/tastebuds/webapp/Storage/PairingStorage.java
@@ -1,11 +1,9 @@
 package com.tastebuds.webapp.Storage;
 
-import com.tastebuds.webapp.resources.Ingredient;
 import com.tastebuds.webapp.resources.Pairing;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
 
 
 @Service
@@ -25,6 +23,10 @@ public class PairingStorage {
     }
 
     public List<Pairing> retrieveAllPairingsByName(String pairing){
+        return pairingRepo.findAllByName(pairing);
+    }
+
+    public Pairing retrievePairingByName(String pairing){
         return pairingRepo.findByName(pairing);
     }
 
--- backEndApi/src/main/java/com/tastebuds/webapp/controllers/PairingController.java
+++ backEndApi/src/main/java/com/tastebuds/webapp/controllers/PairingController.java
@@ -1,22 +1,31 @@
 package com.tastebuds.webapp.controllers;
 
+import com.tastebuds.webapp.Storage.IngredientStorage;
 import com.tastebuds.webapp.Storage.PairingStorage;
+import com.tastebuds.webapp.Storage.PairingsRepository;
 import com.tastebuds.webapp.resources.Ingredient;
 import com.tastebuds.webapp.resources.Pairing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.Query;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class PairingController {
 
-    private PairingStorage pairingStorage;
 
-    public PairingController(PairingStorage pairingStorage) {
+    private PairingStorage pairingStorage;
+    private IngredientStorage ingredientStorage;
+    private PairingsRepository pairingRepo;
+
+    public PairingController(PairingStorage pairingStorage, IngredientStorage ingredientStorage, PairingsRepository pairingRepo) {
         this.pairingStorage = pairingStorage;
+        this.ingredientStorage = ingredientStorage;
+        this.pairingRepo = pairingRepo;
     }
 
     @GetMapping("/api/pairing/{id}")
@@ -36,5 +45,20 @@ public class PairingController {
         return pairingStorage.retrieveAllPairingsByName(pairing);
     }
 
+    @GetMapping("/api/pairings/{pairing}/ingredient")
+    public Iterable<Ingredient> retrieveIngredientsAssociatedWithPairingByName(@PathVariable String pairing){
+       Pairing retrievedPairing = pairingStorage.retrievePairingByName(pairing);
+//       retrievedPairing.getName().equalsIgnoreCase(pairing);
+//        retrievedPairing.getName().compareTo(pairing)
+        return retrievedPairing.getIngredients()
+                .stream()
+                .filter(name -> retrievedPairing.getName().equalsIgnoreCase(pairing))
+                .collect(Collectors.toList());
+    }
 
+
+//return ingredient.getPairings()
+//        .stream()
+//                .filter(pairing -> pairing.getDietaryFilter().equals(filter))
+//            .collect(Collectors.toList());
 }
